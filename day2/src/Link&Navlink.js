f6cd8d935caa85468e0496167b94821b331f0437
Lavanya Miranam <83878776+Lavanya-1222@users.noreply.github.com>
2023-03-13 05:10:53 +0530

    experimented

--- day2/src/Link&Navlink.js
+++ day2/src/Link&Navlink.js
@@ -2,7 +2,8 @@ import {Link,NavLink} from 'react-router-dom'
 // import './App.css'
 
 function LinkNavlink (){
-    return <><Link to='/about' >About</Link><NavLink to='/'  activeclassname="active">Home</NavLink>
+    return <><Link to='/about' activeclassname="active">About</Link>
+    <NavLink to='/'  activeclassname="active">Home</NavLink>
     <Link to='/uselocation'   state={{example:"lava"}}>Uselocation</Link>
         </> 
 }
