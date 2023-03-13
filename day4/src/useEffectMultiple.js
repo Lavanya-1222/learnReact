04959c0c167213b01e3b5e350e009ec70f574646
Lavanya Miranam <83878776+Lavanya-1222@users.noreply.github.com>
2023-03-13 06:16:00 +0530

    use effect on multiple useEffect

--- day4/src/useEffectMultiple.js
+++ day4/src/useEffectMultiple.js
@@ -1,7 +1,6 @@
 import React,{useEffect} from 'react'
 function UseEffectMultiple(){
 
-
     useEffect(()=>console.log("useffect"))
     useEffect(()=>console.log("useffect second"))
     return <><h1>UseEffectMultiple</h1></>
