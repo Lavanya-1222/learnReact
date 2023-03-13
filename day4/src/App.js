5ccea1351049d0f33c21eceefed5ae460f816f23
Lavanya Miranam <83878776+Lavanya-1222@users.noreply.github.com>
2023-03-13 06:43:32 +0530

    useEffect on props and state for particular condition

--- day4/src/App.js
+++ day4/src/App.js
@@ -3,11 +3,11 @@ import UseEffectAfterUpdate from "./useEffectAfterUpdate";
 import UseEffectMultiple from "./useEffectMultiple";
 import UseEffectOnCondition from "./useEffectOnCondition";
 function App(){
-
+let address="solapur";
   return <><UseEffect/>
   <UseEffectAfterUpdate/>
   <UseEffectMultiple/>
-  <UseEffectOnCondition/>
+  <UseEffectOnCondition data={address}/>
   </>
 }
 export default App
--- day4/src/useEffectOnCondition.js
+++ day4/src/useEffectOnCondition.js
@@ -1,13 +1,17 @@
 import React,{useEffect,useState} from 'react'
-function UseEffectOnCondition(){
+function UseEffectOnCondition(props){
 
 const [name,setName]=useState("lava");
 const [age,setAge]=useState(10);
+const[address,setAddress]=useState("")
+    useEffect(()=>console.log("useffect on name state "),[name])
+    useEffect(()=>console.log("useffect on props "),[address])
 
-    useEffect(()=>console.log("useffect on name "),[name])
-    return <><h1>{name} {age}</h1>
+
+    return <><h1>{name} {age} {props.data}</h1>
     <button onClick={()=>{setName("lavanya")
-    setAge(22)}}>UseEffectOnCondition</button>
+    setAge(22)}}>change name and age</button>
+    <button onClick={()=>setAddress(props.data)}>change address</button>
     </>
 }
 export default UseEffectOnCondition
